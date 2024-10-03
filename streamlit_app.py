779af39721022b1a0226e348d12f564f4823feea
samadip91 <45196107+samadip91@users.noreply.github.com>
2024-10-03 13:34:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.title("Hello, World!")
+import folium
+from streamlit_folium import folium_static
+
+# Buat peta dasar
+m = folium.Map(location=[-37.8136, 144.9631], zoom_start=13)
+
+# Tambahkan marker
+folium.Marker([-37.8136, 144.9631], popup="Melbourne").add_to(m)
+
+# Tampilkan peta di Streamlit
+folium_static(m)
